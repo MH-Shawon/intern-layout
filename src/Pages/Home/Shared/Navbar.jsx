a8e48e8d0f50531469930e1e953e3c3197d7dc2a
Mohsin-Hossain <mohsinshawon18@gmail.com>
2023-06-12 23:10:38 +0600

    added

--- src/Pages/Home/Shared/Navbar.jsx
+++ src/Pages/Home/Shared/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () =>
         <div style={{
             background:`url(${appointment})`
           }} className="navbar text-white">
-            <div className="navbar-end">
+            <div className="navbar-start">
                 <div className="dropdown text-black ">
-                    <label tabIndex="0" className="btn btn-ghost lg:invisible ">
+                    <label tabIndex="0" className="btn btn-ghost lg:invisible dropdown-end  ">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
